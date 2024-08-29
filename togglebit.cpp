667170039bf8c67086ae61987d0ba14feaabb251
jayavarth <123160124+jayavarth@users.noreply.github.com>
2024-08-29 21:31:58 +0530

    Delete togglebit.cpp

--- togglebit.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int n,i;
-    cout<<"Enter the number abd the i th bit to be toggled\n";
-    cin>>n>>i;
-    n=n^(1<<i);
-    cout<<"toggled value:"<<n;
-}
